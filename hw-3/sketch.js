7800b2b4827a0d13735ec9558d78288641e7b69b
Marc Lenahan <marc.lenahan@umconnect.umt.edu>
2017-09-26 01:35:04 -0600

    added hw part1

---
+++ hw-3/sketch.js
@@ -0,0 +1,19 @@
+
+// Write initial setup function
+function setup(){
+
+  // Creating the canvas
+  createCanvas( 1280, 760 );
+
+  // Setting canvas color to pink
+  background( 'blue' );
+
+}
+
+// Declaring the draw function
+function draw(){
+
+// Using text function to write out "hello world"
+  text( "Hello World", 300, 200);
+
+}
