ece2f3a9308e139daacf59ca32f135cc20f025b0
Marc Lenahan <marc.lenahan@umconnect.umt.edu>
2017-12-12 12:26:28 -0700

    updated hw12 folder

--- hw-12/sketch.js
+++ hw-12/sketch.js
@@ -1,38 +1,125 @@
-
+let blocks = [];
+let player;
+let numOfBlocks = 1;
+let numOfBlocksDest = 0;
+let numOfBlocksMiss = 0;
 
 function setup(){
-  createCanvas(500,500);
+  createCanvas(windowWidth, windowHeight);
 
+  player = new Player(10);
 
-  ball = new Ball(250,250);
+  for(let i = 0; i < numOfBlocks; i++){
+    let x = width;
+    let y = random(height);
+    let w = random(10);
+    let h = random(25,50);
+    blocks[i] = new Block(x, y, w, h);
+  }
+
 }
 
 function draw(){
+  noCursor();
   background(150);
-  ball.move();
-  ball.display();
-}
 
+  player.update();
+  player.show();
+  player.touch();
+  for(let i = blocks.length - 1; i >= 0; i--){
+    blocks[i].show();
+    blocks[i].move();
+    if(blocks[i].done()){
+      blocks.splice(i,1);
+      numOfBlocksMiss++;
 
+    }
 
-
-class Ball{
-  constructor(x,y){
-    this.x = x;
-    this.y = y;
-    this.x_delta = 0;
-    this.y_delta = 0;
-    this.r = 10;
   }
 
-  display(){
-    rect(this.x,this.y, this.r,this.r);
+  while(blocks.length < numOfBlocks){
+      let x = width;
+      let y = random(height);
+      let w = random(10);
+      let h = random(25,50);
+      blocks.push(new Block(x, y, w, h));
+  }
+
+  push();
+  stroke(0)
+  fill(255);
+  rect(0,0, 250, 50);
+  fill(0);
+  text("Score: " + numOfBlocksDest, 0, 10);
+  text("Blocks Missed: " + numOfBlocksMiss, 0, 20);
+  pop();
+
+}
+
+class Block{
+  constructor(x, y, w, h){
+    this.x = x;
+    this.x_delta = random(-2, 0);
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
+
+  show(){
+    fill(0, 0, random(255));
+    rectMode(CENTER)
+    rect(this.x, this.y, this.w, this.h);
   }
 
   move(){
-    this.x_delta = this.x += -1;
-    if(this.x_delta < 0){
-    this.x = width;
+    this.x += this.x_delta;
+
   }
+
+  done(){
+    if(this.x < 0){
+      return true;
+    }else {
+      return false;
+    }
   }
+}
+
+
+class Player{
+  constructor(r){
+    this.x = mouseX;
+    this.y = mouseY;
+    this.r = r;
+  }
+
+  update(){
+    this.x = mouseX;
+    this.y = mouseY;
+  }
+
+  show(){
+    fill(0);
+    ellipse(this.x, this.y, this.r * 2);
+  }
+
+  touch(){
+    for(let i = blocks.length - 1; i >= 0; i-- ){
+      let d = dist(this.x, this.y, blocks[i].x, blocks[i].y);
+      let thresh = this.r + max([blocks[i].w, blocks[i].h])/2;
+      if(d < thresh){
+        blocks.splice(i,1);
+        numOfBlocks++;
+        numOfBlocksDest++;
+        if(numOfBlocksDest >= 100){
+          blocks[i].x_delta = random(-4,0);
+        } else if(numOfBlocksDest >= 200){
+          blocks[i].x_delta = random(-6,0);
+        }
+      }
+
+    }
+  }
+
+
 }
